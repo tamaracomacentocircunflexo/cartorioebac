cf534f06cc9cae72523448d571b3b103af9bada0
tamaracomacentocircunflexo <147771180+tamaracomacentocircunflexo@users.noreply.github.com>
2023-10-12 19:12:08 -0300

    Opção de Saída
    Incluso a opção de saída para o usuário

--- modulo 5 aula 3 revisado2.c
+++ modulo 5 aula 3 revisado2.c
@@ -128,11 +128,12 @@ int main ()
 	  
 		setlocale(LC_ALL, "Portuguese"); //Definindo linguagem
 	
- 		printf ("### Cartório da EBAC ###\n\n"); //Início do menu
-		printf ("Escolha a opção desejada do menu\n\n");
-		printf ("\t1 - Registrar Nomes\n ");
-		printf ("\t2 - Consultar Nomes\n");
-		printf ("\t3 - Deletar Nomes\n\n"); 
+ 		printf("### Cartório da EBAC ###\n\n"); //Início do menu
+		printf("Escolha a opção desejada do menu\n\n");
+		printf("\t1 - Registrar Nomes\n ");
+		printf("\t2 - Consultar Nomes\n");
+		printf("\t3 - Deletar Nomes\n\n");
+		printf("\t4 - Sair do sistema\n\n"); 
 		printf("Opção: "); //Fim do menu
 	
 		scanf("%d", &opcao); //Armazenando a escolha do usuário
@@ -152,6 +153,11 @@ int main ()
 			
 			case 3:
 		    deletar();
+		    break;
+		    
+		    case 4:
+		    printf("Obrigado por utilizar o sistema!\n");
+		    return 0;
 		    break;
 			
 			default:
